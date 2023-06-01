c1403be168403f78f540d66654edd3698a0551e1
LalremLian <lalremlian44@gmail.com>
2023-06-01 16:17:15 +0600

    Worked on FeedbackForm

--- components/FeedbackForm.js
+++ components/FeedbackForm.js
@@ -1,49 +1,76 @@
 import React, { useState } from "react";
-import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  KeyboardAvoidingView,
+  Alert,
+} from "react-native";
 
 const FeedbackForm = () => {
   // declare the variables
   const [firstName, onChangeFirstName] = useState("");
-  const [lastName, onChangeLastName] = useState("");
+  const [pasword, onChangePassword] = useState("");
   const [message, onChangeMessage] = useState("");
   const [phoneNumber, onChangePhoneNumber] = useState("");
 
   return (
     <View style={styles.container}>
-      <ScrollView style={styles.scrollView}>
-        <Text style={styles.headingSection}>
-          How was your visit to Little Lemon?
-        </Text>
-        <Text style={styles.infoSection}>
-          Little Lemon is a charming neighborhood bistro that serves simple food
-          and classic cocktails in a lively but casual environment. We would
-          love to hear your experience with us!?
-        </Text>
-        <TextInput
-          style={styles.input}
-          value={firstName}
-          placeholder="First Name"
-          onChangeText={onChangeFirstName}
-        />
-        <TextInput
-          style={styles.input}
-          value={lastName}
-          placeholder="Last Name"
-          onChangeText={onChangeLastName}
-        />
-        <TextInput
-          style={styles.input}
-          value={phoneNumber}
-          placeholder="Phone Number"
-          onChangeText={onChangePhoneNumber}
-        />
-        <TextInput
-          style={styles.messageInput}
-          value={message}
-          placeholder="Message"
-          onChangeText={onChangeMessage}
-        />
-      </ScrollView>
+      {/* Wrap the TextInput components in a KeyboardAvoidingView component to ensure that the keyboard does not cover the input fields when the user is typing. */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "android" ? "padding" : "position"}
+      >
+        {/* keyboardDismissMode is used to dismiss the keyboard when the user */}
+        <ScrollView style={styles.scrollView} keyboardDismissMode="on-drag">
+          <Text style={styles.headingSection}>
+            How was your visit to Little Lemon?
+          </Text>
+          <Text style={styles.infoSection}>
+            Little Lemon is a charming neighborhood bistro that serves simple
+            food and classic cocktails in a lively but casual environment. We
+            would love to hear your experience with us!?
+          </Text>
+
+          <TextInput
+            style={styles.input}
+            value={firstName}
+            placeholder="First Name"
+            onFocus={() => {
+              Alert.alert("First name is focussed");
+            }}
+            onBlur={() => {
+              Alert.alert("Thanks for entering your first name");
+            }}
+            onChangeText={onChangeFirstName}
+          />
+          <TextInput
+            style={styles.input}
+            value={pasword}
+            placeholder="Password"
+            secureTextEntry={true}
+            passwordRules={"required: upper; required: lower; required: digit;"}
+            onChangeText={onChangePassword}
+          />
+          <TextInput
+            style={styles.input}
+            value={phoneNumber}
+            placeholder="Phone Number"
+            keyboardType={"phone-pad"}
+            onChangeText={onChangePhoneNumber}
+          />
+          <TextInput
+            style={styles.messageInput}
+            value={message}
+            placeholder="Message"
+            multiline={true}
+            maxLength={10}
+            onChangeText={onChangeMessage}
+          />
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 };
